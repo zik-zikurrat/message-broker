19e53b438abef362b0d14987c02fddb6076bfa47
Zak <polevchshikovzak@gmail.com>
2026-08-03 22:13:51 +0500

    add tests to protocol

--- internal/network/protocol/protocol_test.go
+++ internal/network/protocol/protocol_test.go
@@ -27,7 +27,7 @@ func TestMessageSendSuccess(t *testing.T) {
 	}
 }
 
-func TestMessageSendFailedVersion(t *testing.T) {
+func TestMessageSendFailed(t *testing.T) {
 	var buf bytes.Buffer
 	p := initializeProtocol()
 	p.Encode(&buf, &Message{Version: 2, Type: TypePing})
@@ -40,6 +40,36 @@ func TestMessageSendFailedVersion(t *testing.T) {
 		}
 	}
 }
-func TestDecodeMessage(t *testing.T) {
+func TestEncodeMessageWithPayloadSuccess(t *testing.T) {
+	var buf bytes.Buffer
+	p := initializeProtocol()
+	if err := p.Encode(&buf, &Message{Version: 1, Type: TypeData, Payload: []byte("Test message")}); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
 
+func TestDecodeMessageWithPayloadSuccess(t *testing.T) {
+	var buf bytes.Buffer
+	p := initializeProtocol()
+	payload := bytes.Repeat([]byte{'X'}, 1048576)
+	if err := p.Encode(&buf, &Message{Version: 1, Type: TypeData, Payload: payload}); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	_, err := p.Decode(&buf)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestEncodeMessageWithPayloadFailed(t *testing.T) {
+	var buf bytes.Buffer
+	p := initializeProtocol()
+	payload := bytes.Repeat([]byte{'X'}, 1048577)
+	if err := p.Encode(&buf, &Message{Version: 1, Type: TypeData, Payload: payload}); err != nil {
+		if errors.Is(err, ErrPayloadTooLarge) {
+			return
+		} else {
+			t.Fatalf("unexpected error: expect ErrPayloadTooLarge, got: %v", err)
+		}
+	}
 }
